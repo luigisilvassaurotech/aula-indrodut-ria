ff0226e37b1a5d1fa47cb977e639fac24ad6dba4
luigisilvassaurotech <luigi.silva.santos@escola.pr.gov.br>
2026-08-14 13:47:01 +0000

    Atualização no main.js

---
+++ main.js
@@ -0,0 +1,8 @@
+// Selecao de elementos
+const mario = document.QuerySelector(.mario)
+const pipe = document.QuerySelector(.pipe)
+const startButton = document.QuerySelector(.startButton)
+const restartButton = document.QuerySelector(.restartButton)
+const gameOver = document.QuerySelector(.gameOver)
+// Definicao de audio
+const audioStart = new audio('soung/audio_theme.mp3')
