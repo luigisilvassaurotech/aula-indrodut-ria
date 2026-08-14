005c359f5fafd410dd2fdec96139ed7c7ed5462c
luigisilvassaurotech <luigi.silva.santos@escola.pr.gov.br>
2026-08-14 14:57:25 +0000

    Atualização de main.js

--- main.js
+++ main.js
@@ -6,3 +6,16 @@ const restartButton = document.QuerySelector(.restartButton)
 const gameOver = document.QuerySelector(.gameOver)
 // Definicao de audio
 const audioStart = new audio('soung/audio_theme.mp3')
+// variaveis de controle de jogo
+let gameInternal = null
+// funcao para iniciar o jogo
+const startGame = () => {
+    pipe.classList.add('pipe-animation');
+    startButton.style.display = 'none'
+     gameOver.style.display = 'none'
+     audioStart.currentTime = 0;
+     audioStart.play();
+      if(gameInternal) {
+        gameInterval.setInterval(gameLoop, 10);
+      }
+};
